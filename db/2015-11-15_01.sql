25abac82da5cbf351b91e4fb6181a682689bb90b
Владислав <happy0wnage@gmail.com>
2015-11-15 22:28:27 +0200

    add sql dump

--- db/2015-11-15_01.sql
+++ db/2015-11-15_01.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.8-rc, for Win64 (x86_64)
 --
 -- Host: localhost    Database: park
 -- ------------------------------------------------------
--- Server version	5.6.24-log
+-- Server version	5.7.8-rc-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,15 +23,15 @@ DROP TABLE IF EXISTS `attraction`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `attraction` (
-  `idAttraction` int(11) NOT NULL AUTO_INCREMENT,
+  `id_attraction` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(45) NOT NULL,
   `description` text NOT NULL,
   `height` int(11) NOT NULL,
   `att_picture` text NOT NULL,
   `price_adult` int(11) NOT NULL,
   `price_child` int(11) NOT NULL,
-  PRIMARY KEY (`idAttraction`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id_attraction`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,62 +40,8 @@ CREATE TABLE `attraction` (
 
 LOCK TABLES `attraction` WRITE;
 /*!40000 ALTER TABLE `attraction` DISABLE KEYS */;
+INSERT INTO `attraction` VALUES (1,'Best','Best attraction ever',31,'D:\\Images\\lNmNhbLUHU.mypic',31,14),(2,'New ','New',60,'D:\\Images\\fVVPKqLQxr.mypic',51,15),(3,'Super','Super',90,'D:\\Images\\HHATxfCymu.mypic',80,90),(4,'Ulet','Ulet',90,'D:\\Images\\oFqerPNRqe.mypic',80,89),(5,'Banzai','Banzai',90,'D:\\Images\\ihctbMUUoD.mypic',80,31);
 /*!40000 ALTER TABLE `attraction` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `cart`
---
-
-DROP TABLE IF EXISTS `cart`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `cart` (
-  `idCart` int(11) NOT NULL AUTO_INCREMENT,
-  `idUser` int(11) NOT NULL,
-  PRIMARY KEY (`idCart`),
-  KEY `fk_Cart_User1_idx` (`idUser`),
-  CONSTRAINT `fk_Cart_User1` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cart`
---
-
-LOCK TABLES `cart` WRITE;
-/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `cart_item`
---
-
-DROP TABLE IF EXISTS `cart_item`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `cart_item` (
-  `idCart_Item` int(11) NOT NULL AUTO_INCREMENT,
-  `idCart` int(11) NOT NULL,
-  `idAttraction` int(11) NOT NULL,
-  `adult_quant` int(11) NOT NULL,
-  `child_quant` int(11) NOT NULL,
-  PRIMARY KEY (`idCart_Item`),
-  KEY `fk_Cart_Item_Cart1_idx` (`idCart`),
-  KEY `fk_Cart_Item_Attraction1_idx` (`idAttraction`),
-  CONSTRAINT `fk_Cart_Item_Attraction1` FOREIGN KEY (`idAttraction`) REFERENCES `attraction` (`idAttraction`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Cart_Item_Cart1` FOREIGN KEY (`idCart`) REFERENCES `cart` (`idCart`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cart_item`
---
-
-LOCK TABLES `cart_item` WRITE;
-/*!40000 ALTER TABLE `cart_item` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cart_item` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -106,17 +52,17 @@ DROP TABLE IF EXISTS `comment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `comment` (
-  `idComment` int(11) NOT NULL AUTO_INCREMENT,
-  `idUser` int(11) NOT NULL,
-  `idNews` int(11) NOT NULL,
+  `id_comment` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_news` int(11) NOT NULL,
   `body` varchar(130) NOT NULL,
   `wrote_date` datetime NOT NULL,
-  PRIMARY KEY (`idComment`),
-  KEY `userrId_idx` (`idUser`),
-  KEY `news_id_idx` (`idNews`),
-  CONSTRAINT `news_id` FOREIGN KEY (`idNews`) REFERENCES `news` (`idNews`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `userrId` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id_comment`),
+  KEY `userrId_idx` (`id_user`),
+  KEY `news_id_idx` (`id_news`),
+  CONSTRAINT `news_id` FOREIGN KEY (`id_news`) REFERENCES `news` (`id_news`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `userrId` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,6 +71,7 @@ CREATE TABLE `comment` (
 
 LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (2,1,1,'Круто','2015-11-09 13:47:51');
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -136,15 +83,15 @@ DROP TABLE IF EXISTS `news`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `news` (
-  `idNews` int(11) NOT NULL AUTO_INCREMENT,
-  `idUser` int(11) NOT NULL,
+  `id_news` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
   `title` text NOT NULL,
   `body` text NOT NULL,
   `news_picture` text NOT NULL,
   `post_date` datetime NOT NULL,
-  PRIMARY KEY (`idNews`),
-  KEY `userId_idx` (`idUser`),
-  CONSTRAINT `userId` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  PRIMARY KEY (`id_news`),
+  KEY `userId_idx` (`id_user`),
+  CONSTRAINT `userId` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='				';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -154,7 +101,7 @@ CREATE TABLE `news` (
 
 LOCK TABLES `news` WRITE;
 /*!40000 ALTER TABLE `news` DISABLE KEYS */;
-INSERT INTO `news` VALUES (1,1,'News','News','none picture','2015-12-10 00:00:00');
+INSERT INTO `news` VALUES (1,1,'ПЕРВЫЙ В УКРАИНЕ ФЕСТИВАЛЬ ЗОМБИ ПРОЙДЕТ В ПАРКЕ ГОРЬКОГО','В этом году в рамках традиционного празднования \"Жуткого Хэллоуина в парке\" состоится ЗомбиFEST. Мероприятие, связанное с популярной сейчас темой оживших мертвецов, пройдет впервые в Украине. ЗомбиFEST влючает в себя массу тематических активностей и развлечений.\n\nТак, на аллее у Развлекательного центра разместится Ярмарка Ужасов и Станция Превращения в Зомби. На Средневековой площади пройдет турнир \"Зомби против людей\", в Логове Билли Кида - интерактивная игра \"Охота на Зомби\".\n\nУчастники конкурсов и состязаний получат призы от парка Горького и наших партнеров - Планеты кино ІМАХ, Харьковского дельфинария \"Немо\", квест-комнаты \"Взаперти\", Харьковского цирка.\n\nВечером по Центральной аллее пройдет массовое шествие зомби, в Ретро-парке - киномарафон фильмов ужасов, а на Главной сцене состоится праздничная шоу-программа с тематическими конкурсами, фаершоу и дискотекой.','D:\\Images\\News\\gHSdGLdyDJ.mypic','2015-10-15 22:56:46');
 /*!40000 ALTER TABLE `news` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -166,11 +113,11 @@ DROP TABLE IF EXISTS `role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `role` (
-  `idRole` int(11) NOT NULL AUTO_INCREMENT,
+  `id_role` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
-  PRIMARY KEY (`idRole`),
+  PRIMARY KEY (`id_role`),
   UNIQUE KEY `name_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='		';
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='		';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -179,68 +126,8 @@ CREATE TABLE `role` (
 
 LOCK TABLES `role` WRITE;
 /*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` VALUES (1,'admin'),(2,'user');
+INSERT INTO `role` VALUES (1,'admin'),(3,'editor'),(2,'user');
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `ticket`
---
-
-DROP TABLE IF EXISTS `ticket`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ticket` (
-  `idTicket` int(11) NOT NULL AUTO_INCREMENT,
-  `idAttraction` int(11) NOT NULL,
-  `idTransaction` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
-  `buy_date` datetime NOT NULL,
-  `valid_date` datetime NOT NULL,
-  PRIMARY KEY (`idTicket`),
-  KEY `attractionId_idx` (`idAttraction`),
-  KEY `fk_Ticket_Transaction1_idx` (`idTransaction`),
-  CONSTRAINT `attractionId` FOREIGN KEY (`idAttraction`) REFERENCES `attraction` (`idAttraction`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Ticket_Transaction1` FOREIGN KEY (`idTransaction`) REFERENCES `transaction` (`idTransaction`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ticket`
---
-
-LOCK TABLES `ticket` WRITE;
-/*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `transaction`
---
-
-DROP TABLE IF EXISTS `transaction`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `transaction` (
-  `idTransaction` int(11) NOT NULL AUTO_INCREMENT,
-  `idUser` int(11) NOT NULL,
-  `transaction_date` datetime NOT NULL,
-  `amount` int(11) NOT NULL,
-  `transaction_status` varchar(45) NOT NULL,
-  `ticket_count` int(11) NOT NULL,
-  PRIMARY KEY (`idTransaction`),
-  KEY `fk_Transaction_User1_idx` (`idUser`),
-  CONSTRAINT `fk_Transaction_User1` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `transaction`
---
-
-LOCK TABLES `transaction` WRITE;
-/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
-/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -251,8 +138,8 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `idUser` int(11) NOT NULL AUTO_INCREMENT,
-  `idRole` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `id_role` int(11) NOT NULL,
   `login` varchar(30) NOT NULL,
   `password` varchar(16) NOT NULL,
   `first_name` varchar(60) NOT NULL,
@@ -263,11 +150,11 @@ CREATE TABLE `user` (
   `status_banned` tinyint(1) NOT NULL,
   `dob` date NOT NULL,
   `salt` varchar(256) NOT NULL,
-  PRIMARY KEY (`idUser`),
+  PRIMARY KEY (`id_user`),
   UNIQUE KEY `login_UNIQUE` (`login`),
   UNIQUE KEY `email_UNIQUE` (`email`),
-  KEY `idRole` (`idRole`),
-  CONSTRAINT `idRole` FOREIGN KEY (`idRole`) REFERENCES `role` (`idRole`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `idRole` (`id_role`),
+  CONSTRAINT `idRole` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -277,7 +164,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,1,'admin','1234','Vladyslav','Petrov','happy0wnage@gmail.com','+380930243946','2015-10-12 00:00:00',0,'1995-07-12','1');
+INSERT INTO `user` VALUES (1,1,'admin','1234','admin','admin','admin@mail.ru','+38056789123','2015-11-08 00:00:00',0,'1991-06-03','123');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -290,4 +177,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-11-15 22:05:59
+-- Dump completed on 2015-11-15 22:27:20
